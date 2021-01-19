9eb4fea70533ac76ae83fedd44ffbe7213972431
Michael Oglesby <mboglesby@gmail.com>
2021-01-19 16:03:56 +0000

    Switch to Chrome driver

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # personal-automation
-Personal automation scripts/programs.
 
-### Dockerfile-town_of_cary_payment
-This Dockerfile can be used to build a docker image containing the town_of_cary_payment.py script and all dependencies. This eliminates the need to install the dependencies within your local environment.
+This repo contains a Python script that can be used to issue a payment for a Town of Cary (NC) utility bill. This script can be built into a docker image that includes all required dependencies. 
 
-To build the docker image, run:
+To build the docker image, execute the following steps:
+
+1. Replace the contents of 'town_of_cary_login.py' with your Town of Cary Utiilities login details.
+
+2. Build the docker image.
 
 `docker build -t town_of_cary_payment:20.04 -f Dockerfile-town_of_cary_payment .`
 * Note: 'Dockerfile-town_of_cary_payment', 'town_of_cary_payment.py', and 'town_of_cary_login.py' must all be located in the same directory, and the build command must be run from within that directory.
@@ -15,37 +17,16 @@ Once the docker image is built, you can use it to to execute the town_of_cary_pa
 
 -- or --
 
+`docker run -it --rm town_of_cary_payment --amount <payment_amount_in_usd> --card <payment_card>`
+
+-- or --
+
+`docker run -it --rm town_of_cary_payment --amount <payment_amount_in_usd> --card-label <payment_card_radio_button_label>`
+
+-- or --
+
 `docker run -it --rm town_of_cary_payment:20.04 -a <payment_amount_in_usd> -l <payment_card_radio_button_label>
 
 ..for example:
 `docker run -it --rm town_of_cary_payment:20.04 -a 10.00 -c mo_ofcu`
 
-
-### town_of_cary_payment.py
-DESCRIPTION:    Issues a payment to my Town of Cary (NC) utility bill.
-
-USAGE:          
-
-`python3 ./town_of_cary_payment.py -a <payment_amount_in_usd> -c <payment_card>`
-
--- or --
-
-`python3 ./town_of_cary_payment.py --amount <payment_amount_in_usd> --card <payment_card>`
-
-..for example:
-`python3 ./town_of_cary_payment.py -a 10.00 -c mo_ofcu`
-
-DEPENDENCIES:   
-* Python3 Packages:   selenium, pyvirtualdisplay
-  * Note: You can install these via pip3.
-* Applications:       Firefox, Mozilla Geckodriver, Xvfb (X virtual framebuffer, used by pyvirtualdisplay)
-  * Note: If you are running ubuntu, you can install Firefox and Xvfb via apt.
-  * Note: geckodriver can be installed from here: https://github.com/mozilla/geckodriver/releases
-
-NOTES:          
-* <payment_card> must be a key in the cardRadioButtonLabels dictionary (stored in this script). This dictionary matches a payment card 'nickname' (defined by you; can be anything) to the corresponding radio button label on the Town of Cary's payment site. You will have to get the radio button label from the payment site's html code.
-* 'town_of_cary_login.py' must be located in the same directory as 'town_of_cary_payment.py'. Username and password must be filled in in 'town_of_cary_login.py'. See 'town_of_cary_login.py' for details.
-
-
-### town_of_cary_login.py
-This file contains Town of Cary login details in python dictionary format.  It is used by 'town_of_cary_payment.py'.
--- town_of_cary_payment.py
+++ town_of_cary_payment.py
@@ -18,20 +18,15 @@ Created by Michael Oglesby for personal use.
 
 DESCRIPTION:    Issues a payment to my Town of Cary (NC) utility bill.
 
-USAGE:          python3 ./town_of_cary_payment.py -a <payment_amount_in_usd> -c <payment_card>
+USAGE:          docker run -it --rm town_of_cary_payment -a <payment_amount_in_usd> -c <payment_card>
                     -- or --
-                python3 ./town_of_cary_payment.py --amount <payment_amount_in_usd> --card <payment_card>
+                docker run -it --rm town_of_cary_payment --amount <payment_amount_in_usd> --card <payment_card>
                     -- or --
-                python3 ./town_of_cary_payment.py --amount <payment_amount_in_usd> --card-label <payment_card_radio_button_label>
+                docker run -it --rm town_of_cary_payment --amount <payment_amount_in_usd> --card-label <payment_card_radio_button_label>
                     -- or --
-                python3 ./town_of_cary_payment.py -a <payment_amount_in_usd> -l <payment_card_radio_button_label>
-                    -- or, if executed using docker image built from Dockerfile-town_of_cary_payment --
-                docker run -it --rm town_of_cary_payment -a <payment_amount_in_usd> -c <payment_card>
+                docker run -it --rm town_of_cary_payment -a <payment_amount_in_usd> -l <payment_card_radio_button_label>
 
-EXAMPLE USAGE:  python3 ./town_of_cary_payment.py -a 10.00 -c mo_ofcu
-
-DEPENDENCIES:   Python3 Packages:   selenium, pyvirtualdisplay
-                Applications:       Firefox, Mozilla Geckodriver, Xvfb (X virtual framebuffer, used by pyvirtualdisplay)
+EXAMPLE USAGE:  docker run -it --rm town_of_cary_payment -a 10.00 -c mo_ofcu
 
 NOTES:          
 
@@ -39,8 +34,6 @@ NOTES:
 
 * Current contents of cardRadioButtonLabels dictionary (for reference):
     """ + str(cardRadioButtonLabels) + """
-
-* town_of_cary_login.py must be located in the same directory as town_of_cary_payment.py. Username and password must be filled in in town_of_cary_login.py. See town_of_cary_login.py for details.
 """
 hintsText = """\nHints:
 * Ensure that town_of_cary_login.py contains your login details.")
@@ -104,16 +97,11 @@ if not cardLabel :
 # Import selenium and pyvirtualdisplay
 try :
     from selenium import webdriver
-    from selenium.webdriver.firefox.options import Options
     from selenium.common import exceptions
+    from selenium.webdriver.common.keys import Keys
+    from selenium.webdriver.common.by import By
 except :
     print("Error importing selenium... Perhaps the selenium python3 package is not installed?")
-    print("Note: You can install this package with pip3.")
-    sys.exit(0)
-try :
-    from pyvirtualdisplay import Display
-except :
-    print("Error importing pyvirtualdisplay... Perhaps the pyvirtualdisplay python3 package is not installed?")
     print("Note: You can install this package with pip3.")
     sys.exit(0)
 
@@ -123,42 +111,42 @@ if card :
 else :
     print("Preparing to issue a $" + amount + " payment to the Town of Cary using payment card with label '" + cardLabel + "'...")
 
-# Instantiate virtual display
-print("Starting virtual display...")
+# Instantiate Chrome driver
 try :
-    display = Display(visible=0, size=(3840, 2160))
-    display.start()
+    print("Instantiating Chrome selenium webdriver...")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('headless')
+    chrome_options.add_argument("disable-dev-shm-usage")
+    chrome_options.add_argument('no-sandbox')
+    chrome_options.add_argument("start-maximized")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_argument("--disable-blink-features")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    driver = webdriver.Chrome(options=chrome_options)
 except Exception as e :
-    print("Error starting virtual display (see 'Message' below)... Perhaps Xvfb is not installed?")
-    print("Note: if you are running on ubuntu, you can install this with apt.")
-    print(str(e).strip())
-    sys.exit(0)
-
-# Instantiate Firefox driver
-try :
-    print("Instantiating Firefox selenium webdriver...")
-    options = Options()
-    options.headless = True
-    driver = webdriver.Firefox(options=options)
-except Exception as e :
-    print("Error instantiating Firefox selenium webdriver (see 'Message' below)... Perhaps geckodriver is not installed?")
+    print("Error instantiating Chrome selenium webdriver (see 'Message' below)... Perhaps geckodriver is not installed?")
     print("Note: you can find geckodriver here: https://github.com/mozilla/geckodriver")
-    print("Stopping virtual display...")
-    display.stop()
     print(str(e).strip())
     sys.exit(0)
 
 # Execute payment
 try :
     print("Executing payment...")
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     driver.get("https://ipn.paymentus.com/cp/tcnc")
+    driver.get("https://ipn.paymentus.com/cp/tcnc")
+    time.sleep(2)
     driver.find_element_by_id("id_loginId").click()
-    driver.find_element_by_id("id_loginId").clear()
+    time.sleep(2)
     driver.find_element_by_id("id_loginId").send_keys(townOfCaryLogin["username"])
+    time.sleep(5)
     driver.find_element_by_id("id_password").click()
-    driver.find_element_by_id("id_password").clear()
+    time.sleep(2)
     driver.find_element_by_id("id_password").send_keys(townOfCaryLogin["password"])
+    time.sleep(5)
     driver.find_element_by_xpath("//*[@id=\"main-container\"]/div/div[2]/div/form/div/div[4]/div/input").click()
+    time.sleep(5)
     driver.find_element_by_link_text("Pay Bill").click()
     driver.find_element_by_id("header.paymentAmount").click()
     driver.find_element_by_id("header.paymentAmount").clear()
@@ -180,15 +168,13 @@ except Exception as e :
     print("Closing webdriver and stopping virtual display...")
     driver.close()
     driver.quit()
-    display.stop()
     print(str(e).strip())
     print(hintsText)
     sys.exit(0)
 
-# Close virtual display and Firefox
+# Close virtual display and Chrome
 print("Closing webdriver and stoping virtualdisplay...")
 driver.close()
 driver.quit()
-display.stop()
 
 print("Success!")
